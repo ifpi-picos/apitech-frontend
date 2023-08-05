6590489c1e8b181708d28e4501764ff153c10191
WisleyASousa <wisley.sousa@outlook.com.br>
2023-08-05 12:52:04 -0300

    definindo Tema de Cores

---
+++ src/theme/index.ts
@@ -0,0 +1,37 @@
+export default {
+  COLORS: {
+    WHITE: '#FFFFFF',
+    BLACK: '#000000',
+
+    RED_LIGHT: '#EB7A53',
+    RED_MID: '#F03434',
+    YELLOW: '#F7D44C',
+    BLUE: '#98B7DB',
+    GREEN: '#A8D672',
+    BEIGE: '#F6ECC9',
+
+    GRAY_100: '#E1E1E6',
+    GRAY_200: '#C4C4CC',
+    GRAY_300: '#8D8D99',
+    GRAY_400: '#7C7C8A',
+    GRAY_500: '#505059',
+    GRAY_800: '#202024',
+    GRAY_700: '#29292E',
+    GRAY_600: '#323238',
+
+  },
+  FONT_FAMILY: {
+    REGULAR: 'Roboto_400Regular',
+    BOLD: 'Roboto_700Bold'
+  },
+
+  FONT_SIZE: {
+    XS: 12,
+    SM: 14,
+    MD: 16,
+    LG: 18,
+    XL: 20,
+    XXL: 24,
+    XXXL: 32,
+  }
+};
