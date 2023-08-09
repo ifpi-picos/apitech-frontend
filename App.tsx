b99f38d2623dc9231563017dffe63dfc3c4f37cf
WisleyASousa <wisley.sousa@outlook.com.br>
2023-08-09 11:22:26 -0300

    v1.0.1: configurando font padrao da aplicacao

--- App.tsx
+++ App.tsx
@@ -1,8 +1,22 @@
+import { View, Text } from 'react-native';
 import { SignIn } from './src/screens/SignIn/Index';
+import { useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold, Roboto_900Black } from '@expo-google-fonts/roboto';
+
 
 export default function App() {
+
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_500Medium,
+    Roboto_700Bold,
+    Roboto_900Black
+  });
+
   return (
-    <SignIn />
+    <View>
+      {fontsLoaded ? ( <Text>Fontes carregadas</Text> ) : <View />}
+      <SignIn />
+    </View>
   );
 }
 
