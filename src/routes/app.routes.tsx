8d7f3e080cda40d99965b35b8f80e02673b18629
WisleyASousa <wisley.sousa@outlook.com.br>
2023-08-20 17:28:01 -0300

    v1.3.13: removendo header, alterando icones do Bottom

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,12 +1,19 @@
+import { useTheme } from "native-base";
+
 import { createBottomTabNavigator, BottomTabNavigationProp } from "@react-navigation/bottom-tabs";
+
+import HomeSvg from "../assets/home.svg";
+import ProfileSvg from "../assets/profile.svg";
+import ApiarySvg from "../assets/history.svg";
+
 import { Profile } from "../screens/Profile";
 import { Apiary } from "../screens/Apiary";
 import { Home } from "../screens/Home";
 
 type AppRoutes = {
-  home: undefined;
-  profile: undefined;
-  apiary: undefined;
+  Início: undefined;
+  Perfil: undefined;
+  Apiário: undefined;
 }
 //sao as propriedades das rotas de navegacao da aplicacao.
 export type AppNavigatorRoutesProps = BottomTabNavigationProp<AppRoutes>;
@@ -15,20 +22,43 @@ const { Navigator, Screen } = createBottomTabNavigator<AppRoutes>();
 
 
 export function AppRoutes() {
+  const { sizes } = useTheme();
+
+  const iconSize = sizes[10];
+
   return(
-    <Navigator>
+    <Navigator
+      screenOptions={{ 
+        headerShown: false 
+      }}
+    >
       <Screen
-        name="home"
+        name="Início"
         component={Home}
+        options={{
+          tabBarIcon: (({ color }) => (
+            <HomeSvg width={iconSize} height={iconSize} fill={color} />
+          ))
+        }}
       />
       <Screen
-        name="profile"
-        component={Profile}
-      />
-      <Screen
-        name="apiary"
+        name="Apiário"
         component={Apiary}
+        options={{
+          tabBarIcon: (({ color }) => (
+            <ApiarySvg width={iconSize} height={iconSize} fill={color} />
+            ))
+          }}
       />
+          <Screen
+            name="Perfil"
+            component={Profile}
+            options={{
+              tabBarIcon: (({ color }) => (
+                <ProfileSvg width={iconSize} height={iconSize} fill={color} />
+              ))
+            }}
+          />
     </Navigator>
     )
 }
